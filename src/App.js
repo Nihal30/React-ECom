8e33f49b686dec30cad5e978e960462730cc2dda
Nihal Kishore <nihal301298@gmail.com>
2023-07-22 12:19:41 +0530

    Add Routing to Login/Signp

--- src/App.js
+++ src/App.js
@@ -5,11 +5,36 @@ import ProductList from './features/product-list/ProductList';
 import Home from './pages/Home';
 import LogInPage from './pages/LogInPage';
 import SignUpPage from "./pages/SignUpPage"
+import { createRoot } from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />
+  },
+  {
+    path: "/login",
+    element: <LogInPage/>,
+  },
+  {
+    path: "/signup",
+    element: <SignUpPage/>,
+  },
+]);
+
+
+
 
 function App() {
   return (
     <div className="App">
-    <SignUpPage />
+    <RouterProvider router={router} />
     </div>
   );
 }
